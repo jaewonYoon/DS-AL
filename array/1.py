e74c53577bad0e55bf370d2456bffe38b9078b2d
jaewonYoon <bunggl@donutskorea.com>
2019-07-29 09:54:16 +0900

    important functions

---
+++ array/1.py
@@ -0,0 +1,9 @@
+#set 1  important functions
+import array 
+
+arr = array.array('i', [1, 2, 3])
+print("THe new created array is : ", end="")
+for i in range( 0, 3):
+        print (arr[i], end= " ")
+
+print("\r")
