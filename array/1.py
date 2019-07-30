f0e4a0347fae078eb8c64d87012cb2fadd321817
jaewonYoon <bunggl@donutskorea.com>
2019-07-30 09:37:06 +0900

    list rotate

--- array/1.py
+++ array/1.py
@@ -1,9 +1,5 @@
-#set 1  important functions
-import array 
+def rotate(arr, n ,d):
+        temp1 = arr[n:d]
+        temp2 = arr[0:n]
+        print(temp1 + temp2)
 
-arr = array.array('i', [1, 2, 3])
-print("THe new created array is : ", end="")
-for i in range( 0, 3):
-        print (arr[i], end= " ")
-
-print("\r")
