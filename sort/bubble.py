21a4a3d392e6af9ec6e16ed2f1b6029ee71fb200
jaewonYoon <21300492@handong.edu>
2019-08-07 09:34:43 +0900

    직접 만들어보기

--- sort/bubble.py
+++ sort/bubble.py
@@ -1,12 +1,10 @@
-def BubbleSort(li):
-    list_length = len(li)
-
-    for i in range(list_length-1):
-        for j in range(list_length-i-1):
-            if li[j] > li[j+1]: 
-                li[j], li[j+1] = li[j+1], li[j] 
+def bubbleSort(alist):
+    for i in range(len(alist)-1):
+        for j in range(len(alist)-1-i):
+            if(alist[j+1] < alist[j]):
+                alist[j+1], alist[j] = alist[j], alist[j+1]
 
 if __name__ == "__main__":
-    li = [2,3,1,4]
-    BubbleSort(li)
+    li = [3,2,1,4]
+    bubbleSort(li)
     print(li)
