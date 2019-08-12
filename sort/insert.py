c61785cd3c0b3a07f106345a50bb2943be2f8746
jaewonYoon <21300492@handong.edu>
2019-08-12 10:02:07 +0900

    merge sort

---
+++ sort/insert.py
@@ -0,0 +1,6 @@
+def insertion_sort(li):
+    for i in range(1, len(li)):
+        j = i-1
+        key = li[i]
+        while li[j] > key and :
+            
--- sort/select.py
+++ sort/select.py
@@ -1,13 +1,12 @@
 def selection_sort(li):
     for i in range(len(li)-1):
-        min_idx=i
-        for j in range(i+1,len(li)):
-            if li[min_idx] > li[j]: 
+        min_idx = i
+        for j in range(i+1, len(li)):
+            if(li[min_idx] > li[j]):
                 min_idx = j
-        if min_idx != i:
+        if(i!=min_idx):
             li[i], li[min_idx] = li[min_idx], li[i]
     return li
-    
-if __name__ == "__main__": 
-    for i in range(10): 
-        print(i)
+if __name__ == "__main__":
+    li = [2,5,6,1,3,8,4,3,9,6,10,11,12]
+    print(selection_sort(li))
